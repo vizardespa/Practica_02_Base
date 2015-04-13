b1146a7d8eaecb891d5c57c267cd56d378626cbd
Angel Miguel Arambula Garcia <angel.arambula.g@gmail.com>
2015-04-12 22:56:16 -0700

    Activities creation, action bar menu created.

--- app/src/main/java/com/cetys/angelarambula/android/practica_01_base/MainActivity.java
+++ app/src/main/java/com/cetys/angelarambula/android/practica_01_base/MainActivity.java
@@ -1,7 +1,8 @@
 package com.cetys.angelarambula.android.practica_01_base;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,23 +17,30 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+
+                return true;
+            case R.id.action_greet:
+                greetActivity();
+                return true;
+            case R.id.action_listView:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    private void greetActivity() {
+        Intent intent = new Intent(MainActivity.this, GreetingActivity.class);
+        startActivity(intent);
+    }
+
 }
